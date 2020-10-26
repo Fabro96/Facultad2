b4aebec5593aed946f4b4951c30ea3803e67fd98
Fabrizio Agustín Ghilardini <70611279+Fabro96@users.noreply.github.com>
2020-10-26 19:58:48 -0300

    Commit
    Probando Formularios

---
+++ Solucion.Consola/Solucion.Formulario/FmrFacultad.cs
@@ -0,0 +1,44 @@
+﻿using Solucion.LibreriaNegocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solucion.Formulario
+{
+    public partial class FmrFacultad : Form
+    {
+        //ATRIBUTOS
+        private Facultad _facultad;
+
+        //PROPIEDADES
+        public Facultad Facultad
+        {
+            get { return _facultad; }
+        }
+
+        //CONSTRUCTOR
+        public FmrFacultad(Facultad facultad)
+        {
+            _facultad = facultad;
+
+            InitializeComponent();
+        }
+
+        private void FmrFacultad_Load(object sender, EventArgs e)
+        {
+            this.Text = _facultad.Nombre;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmLista lista = new FrmLista();
+            lista.Show();
+        }
+    }
+}
---
+++ Solucion.Consola/Solucion.Formulario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Solucion.LibreriaNegocio;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solucion.Formulario
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(
+                new FmrFacultad(
+                    new Facultad("UBA - FCE", 3)));
+        }
+    }
+}
